147f38ff25f3bcded4e6ea86317afd9632e35204
Nestor9801 <nestorosielzavaleta980109@gmail.com>
2024-01-04 23:35:51 -0600

    Class Company

---
+++ Company.py
@@ -0,0 +1,16 @@
+class Company:
+ def __init__(self):
+  #Protected number
+  self._project = "NLP"
+
+#child class
+class Employee(Company):
+  def __init__(self,name):
+    self.name = name
+    Company.__init__(self)
+  def show(self):
+    print("Employee name:", self.name)
+    #Accessing protected member in child class
+    print("Working in project:", self._project)
+c= Employee("Jessa")
+c.show()
